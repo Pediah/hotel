2d62f937617f0f5d463a14e0dacec104147784d9
unknown <paulpmeck@gmail.com>
2024-11-29 20:19:41 +0200

    initial

--- js/main.js
+++ js/main.js
@@ -1,19 +1,52 @@
 (function ($) {
     "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
     
-    // Initiate the wowjs animation library
+    
+    // Initiate the wowjs
     new WOW().init();
     
-    // Initiate menu
-    $('#header').after('<div class="mobile-menu d-xl-none">');
-    $('.top-menu').clone().appendTo('.mobile-menu');
-    $('.mobile-menu-btn').click(function () {
-        $('.mobile-menu').stop().slideToggle();
+    
+    // Dropdown on mouse hover
+    const $dropdown = $(".dropdown");
+    const $dropdownToggle = $(".dropdown-toggle");
+    const $dropdownMenu = $(".dropdown-menu");
+    const showClass = "show";
+    
+    $(window).on("load resize", function() {
+        if (this.matchMedia("(min-width: 992px)").matches) {
+            $dropdown.hover(
+            function() {
+                const $this = $(this);
+                $this.addClass(showClass);
+                $this.find($dropdownToggle).attr("aria-expanded", "true");
+                $this.find($dropdownMenu).addClass(showClass);
+            },
+            function() {
+                const $this = $(this);
+                $this.removeClass(showClass);
+                $this.find($dropdownToggle).attr("aria-expanded", "false");
+                $this.find($dropdownMenu).removeClass(showClass);
+            }
+            );
+        } else {
+            $dropdown.off("mouseenter mouseleave");
+        }
     });
+    
     
     // Back to top button
     $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
+        if ($(this).scrollTop() > 300) {
             $('.back-to-top').fadeIn('slow');
         } else {
             $('.back-to-top').fadeOut('slow');
@@ -23,46 +56,54 @@
         $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
         return false;
     });
-    
-    // Date and time picker
-    $('#date-1, #date-2, #date-3, #date-4').datetimepicker({
-        format: 'L'
-    });
-    $('#time-1, #time-2').datetimepicker({
-        format: 'LT'
-    });
-    
-    //Portfolio modal slider
-    $('.port-slider').delay(10000);
-    $('.port-slider').slick({
-        autoplay: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        fade: true,
-        asNavFor: '.port-slider-nav'
-    });
-    $('.port-slider-nav').slick({
-        autoplay: true,
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        asNavFor: '.port-slider',
-        arrows: false,
-        dots: false,
-        centerMode: true,
-        focusOnSelect: true
-    });
-    
-    $('#popover-content-download').hide();
-    $("[data-toggle=popover]").each(function (e) {
-        $(this).popover({
-            html: true,
-            content: function () {
-                var id = $(this).attr('id')
-                return $('#popover-content-' + id).html();
-            }
-        });
 
+
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 2000
     });
+
+
+    // Modal Video
+    $(document).ready(function () {
+        var $videoSrc;
+        $('.btn-play').click(function () {
+            $videoSrc = $(this).data("src");
+        });
+        console.log($videoSrc);
+
+        $('#videoModal').on('shown.bs.modal', function (e) {
+            $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+        })
+
+        $('#videoModal').on('hide.bs.modal', function (e) {
+            $("#video").attr('src', $videoSrc);
+        })
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        margin: 25,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            }
+        }
+    });
+    
 })(jQuery);
 
